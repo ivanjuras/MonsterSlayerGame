4c6010a6712d1d746f5838bcbe136ce4aaa53be9
Ivan Juras <ijuras@gmail.com>
2018-02-19 09:08:34 +0100

    Add the log

--- js/app.js
+++ js/app.js
@@ -10,11 +10,13 @@ new Vue({
       specialMin: 10,
       specialMax: 20
     },
+    playerHealingValue: 7,
     monsterAttackValues: {
       min: 5,
       max: 12
     },
-    gameIsRunning: false
+    gameIsRunning: false,
+    turns: []
   },
 
   methods: {
@@ -22,6 +24,7 @@ new Vue({
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
 
     attack: function() {
@@ -62,10 +65,11 @@ new Vue({
 
     heal: function() {
       if (this.playerHealth <= 93) {
-        this.playerHealth += 7;
+        this.playerHealth += this.playerHealingValue;
       } else {
         this.playerHealth = 100;
       }
+      this.printHealLog(this.playerHealingValue);
 
       this.monsterAttack(
         this.monsterAttackValues.min,
@@ -81,11 +85,15 @@ new Vue({
 
     // Helper functions
     playerAttack: function(min, max) {
-      this.monsterHealth -= this.calculateDamage(min, max);
+      var damage = this.calculateDamage(min, max);
+      this.monsterHealth -= damage;
+      this.printAttackLog('Player', damage);
     },
 
     monsterAttack: function(min, max) {
-      this.playerHealth -= this.calculateDamage(min, max);
+      var damage = this.calculateDamage(min, max);
+      this.playerHealth -= damage;
+      this.printAttackLog('Monster', damage);
     },
 
     calculateDamage: function(min, max) {
@@ -102,6 +110,29 @@ new Vue({
       }
 
       return false;
+    },
+
+    printAttackLog: function(role, damage) {
+      if (role === 'Player') {
+        this.turns.unshift({
+          isPlayer: true,
+          text: 'Player hits Monster for ' + damage
+        });
+      } else if (role === 'Monster') {
+        this.turns.unshift({
+          isPlayer: false,
+          text: 'Monster hits Player for ' + damage
+        });
+      } else {
+        alert('Error!!! Neither the player nor the monster attacked.');
+      }
+    },
+
+    printHealLog: function(healingAmount) {
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player heals for ' + healingAmount
+      });
     }
   }
 });
