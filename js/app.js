77a6e043994681ce6c62897c080aca9d8d4515ab
Ivan Juras <ijuras@gmail.com>
2018-02-19 08:39:53 +0100

    Add special attack and refactor the code

--- js/app.js
+++ js/app.js
@@ -4,6 +4,16 @@ new Vue({
   data: {
     playerHealth: 100,
     monsterHealth: 100,
+    playerAttackValues: {
+      min: 3,
+      max: 10,
+      specialMin: 10,
+      specialMax: 20
+    },
+    monsterAttackValues: {
+      min: 5,
+      max: 12
+    },
     gameIsRunning: false
   },
 
@@ -15,19 +25,39 @@ new Vue({
     },
 
     attack: function() {
-      this.monsterHealth -= this.calculateDamage(3, 10);
+      this.playerAttack(
+        this.playerAttackValues.min,
+        this.playerAttackValues.max
+      );
 
       if (this.checkWin()) {
          return;
       }
 
-      this.playerHealth -= this.calculateDamage(5, 12);
+      this.monsterAttack(
+        this.monsterAttackValues.min,
+        this.monsterAttackValues.max
+      );
 
       this.checkWin();
     },
 
     specialAttack: function() {
+      this.playerAttack(
+        this.playerAttackValues.specialMin,
+        this.playerAttackValues.specialMax
+      );
 
+      if (this.checkWin()) {
+        return;
+     }
+
+     this.monsterAttack(
+      this.monsterAttackValues.min,
+      this.monsterAttackValues.max
+     );
+
+     this.checkWin();
     },
 
     heal: function() {
@@ -39,6 +69,14 @@ new Vue({
     },
 
     // Helper functions
+    playerAttack: function(min, max) {
+      this.monsterHealth -= this.calculateDamage(min, max);
+    },
+
+    monsterAttack: function(min, max) {
+      this.playerHealth -= this.calculateDamage(min, max);
+    },
+
     calculateDamage: function(min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
     },
